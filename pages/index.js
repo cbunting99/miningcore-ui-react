fe1603467dad77c79a9af15fb7c14951d4fed348
Chris Bunting <cbuntingde@gmail.com>
2024-07-19 14:49:58 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ export default function () {
             <Card>
               <Statistic
                 title="poolFeePercent"
-                value={app[0].poolFeePercent || 0, 0, '%'}
+                value={app[0].poolFeePercent || 0, '1 %'}
                 valueStyle={{ color: '#3f8600' }}
               />
             </Card>
